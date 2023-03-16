3b1f96b9eef3d260460f5c5b50c331bbc76b4035
Lukas Bacovsky <l.bacovsky@gmail.com>
2023-03-16 22:36:13 +0100

    cosmetic changes

--- report_file.py
+++ report_file.py
@@ -7,29 +7,32 @@ class ReportFile:
     CSV_COLUMNS = ["DATE", "TIME", "EVENT"]
 
     def __init__(self, path: str) -> None:
-        self.REPORT_FILE_PATH = path
+        self.REPORT_FILE_PATH = Path(path)
         self.create_if_not_exists()
 
     def create_if_not_exists(self) -> None:
-        report_file = Path(self.REPORT_FILE_PATH)
-        file_exists = report_file.exists()
-
-        if file_exists:
+        print(f'Report file: {self.REPORT_FILE_PATH.resolve()}')
+        
+        if self.REPORT_FILE_PATH.exists():
             return None
     
-        Path(self.REPORT_FILE_PATH).touch(exist_ok=False)
+        self.REPORT_FILE_PATH.touch(exist_ok=False)
         with open(self.REPORT_FILE_PATH,
-                    "w",
-                    encoding="utf-8",
-                    newline='') as csvfile:
+                  "w",
+                  encoding="utf-8",
+                  newline='') as csvfile:
             writer = csv.DictWriter(
                 csvfile,
                 fieldnames=self.CSV_COLUMNS,
                 quoting=csv.QUOTE_ALL)
             writer.writeheader()
-        print(f'Report file: {self.REPORT_FILE_PATH}')
 
     def write(self, _date, _time, event) -> None:
+        csv_header = {
+            self.CSV_COLUMNS[0]: _date,
+            self.CSV_COLUMNS[1]: _time,
+            self.CSV_COLUMNS[2]: event
+        }
         with open(self.REPORT_FILE_PATH,
                   "a+",
                   encoding="utf-8",
@@ -38,7 +41,4 @@ class ReportFile:
                 csvfile,
                 fieldnames=self.CSV_COLUMNS,
                 quoting=csv.QUOTE_ALL)
-            writer.writerow({
-                self.CSV_COLUMNS[0]: _date,
-                self.CSV_COLUMNS[1]: _time,
-                self.CSV_COLUMNS[2]: event})
+            writer.writerow(csv_header)
